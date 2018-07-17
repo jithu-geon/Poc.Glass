d9c6485145dd877e427eda443a4120b487b1e04c
Midhun <midhun.gopinath@gadgeon.com>
2018-07-17 16:46:37 +0530

    added container component

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { LayoutModule } from '@glass/layout';
+import { ContainerModule} from '@glass/container';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule,
     LayoutModule, 
+    ContainerModule,
     NxModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
